38d149c5908295e3f4c738e865d6cd4abec867de
forste <stephanfortelny@gmail.com>
2014-05-02 14:42:44 +0200

    working cubes on surface view

--- gen/forste/ourbikes/R.java
+++ gen/forste/ourbikes/R.java
@@ -26,41 +26,53 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_search=0x7f080003;
-        public static final int action_settings=0x7f080002;
+        public static final int action_search=0x7f08000a;
+        public static final int action_settings=0x7f080009;
         public static final int container=0x7f080000;
         public static final int edit_message=0x7f080001;
+        public static final int glsurfaceview=0x7f080002;
+        public static final int gone=0x7f080008;
+        public static final int hidecontainer=0x7f080003;
+        public static final int hideme1=0x7f080004;
+        public static final int hideme2=0x7f080005;
+        public static final int invis=0x7f080007;
+        public static final int vis=0x7f080006;
     }
     public static final class layout {
         public static final int activity_display_message=0x7f030000;
         public static final int activity_main=0x7f030001;
-        public static final int fragment_display_message=0x7f030002;
-        public static final int fragment_main=0x7f030003;
+        public static final int activity_video=0x7f030002;
+        public static final int fragment_display_message=0x7f030003;
+        public static final int fragment_main=0x7f030004;
+        public static final int fragment_video=0x7f030005;
+        public static final int surface_view_overlay=0x7f030006;
     }
     public static final class menu {
         public static final int display_message=0x7f070000;
         public static final int main=0x7f070001;
+        public static final int video=0x7f070002;
     }
     public static final class string {
-        public static final int action_search=0x7f050007;
-        public static final int action_settings=0x7f050003;
-        public static final int app_name=0x7f050000;
-        public static final int button_send=0x7f050002;
-        public static final int edit_message=0x7f050001;
-        public static final int hello_world=0x7f050006;
-        public static final int title_activity_display_message=0x7f050005;
-        public static final int title_activity_main=0x7f050004;
+        public static final int action_search=0x7f050008;
+        public static final int action_settings=0x7f050004;
+        public static final int app_name=0x7f050001;
+        public static final int button_send=0x7f050003;
+        public static final int edit_message=0x7f050002;
+        public static final int hello_world=0x7f050007;
+        public static final int hide_me=0x7f05000a;
+        public static final int title_activity_display_message=0x7f050006;
+        public static final int title_activity_main=0x7f050005;
+        public static final int title_activity_video=0x7f050009;
+        public static final int translucent_background=0x7f050000;
+        public static final int visibility_1_gone=0x7f05000d;
+        public static final int visibility_1_invis=0x7f05000c;
+        public static final int visibility_1_vis=0x7f05000b;
     }
     public static final class style {
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
     
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
 
         Base application theme for API 11+. This theme completely replaces
         AppBaseTheme from res/values/styles.xml on API 11+ devices.
